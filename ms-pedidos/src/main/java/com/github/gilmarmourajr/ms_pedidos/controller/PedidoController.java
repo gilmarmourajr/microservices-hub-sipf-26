8bc4c67b1fcbb23f37d24cd89e8f60489d3701c1
gilmarmourajr <juzinho-gilmar@hotmail.com>
2026-05-13 10:37:20 -0300

    feat: endpoint testar balanceamento

--- ms-pedidos/src/main/java/com/github/gilmarmourajr/ms_pedidos/controller/PedidoController.java
+++ ms-pedidos/src/main/java/com/github/gilmarmourajr/ms_pedidos/controller/PedidoController.java
@@ -4,6 +4,7 @@ import com.github.gilmarmourajr.ms_pedidos.Service.PedidoService;
 import com.github.gilmarmourajr.ms_pedidos.dto.PedidoDTO;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -17,6 +18,11 @@ public class PedidoController {
 
     @Autowired
     PedidoService pedidoService;
+
+    @GetMapping("/port")
+    public String port(@Value("${local.server.port}") String porta) {
+        return "Instância respondeu na porta " + porta;
+    }
 
     @GetMapping
     public ResponseEntity<List<PedidoDTO>> getAllPedidos(){
